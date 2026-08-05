374ebe273dbf21c90b810780c4bbcbbb68793472
doomsower <12031673+doomsower@users.noreply.github.com>
2026-08-05 13:54:18 -0400

    fix: optimist build

--- apps/optimist/esbuild.config.mjs
+++ apps/optimist/esbuild.config.mjs
@@ -36,7 +36,6 @@ build({
     js: `
       import { createRequire } from 'module';
       import { fileURLToPath } from 'url';
-      import path from 'path';
       const require = createRequire(import.meta.url);
       const __filename = fileURLToPath(import.meta.url);
       const __dirname = path.dirname(__filename);
